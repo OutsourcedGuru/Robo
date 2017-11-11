3e83438956f5a6b233fb740605dcd1b8e7b8bf0c
myJS.io Support <info@myJS.io>
2017-11-10 16:14:05 -0800

    v1.0.0 Initial checkin

---
+++ README.md
@@ -0,0 +1,21 @@
+# Robo app 
+
+> [Framework7](http://www.idangero.us/framework7) is a Mobile UI framework that can be used to build hybrid apps with PhoneGap. This app is written in PhoneGap (Cordova) using Framework7. 
+
+> [Robo 3D](https://robo3d.com) is a company in San Diego which makes 3D printers for the consumer market. This app is designed specifically for their printers, specifically the C2 and R2 models. 
+  
+## Overview
+The original Robo beta app provided by Robo 3D was written for the iOS platform and works reasonably well. Although some work has been put into porting this to other smartphones, no app has yet been released.
+
+For this reason, I decided to re-write the entire portable application using a platform which is more conducive to cross-platform development, Adobe PhoneGap (Cordova).
+
+## Progress
+The app looks good so far. I've managed to reasonably mimic the original look and feel after the port to PhoneGap/Framework7.
+
+At the moment, it's "greeked-in" and will need further work now to exercise the underlying [OctoPrint API](http://docs.octoprint.org/en/master/api/) for querying the printer.
+
+Comments/questions?  [info@robo.fyi](mailto:info@robo.fyi)
+
+
+
+  
---
+++ www/js/my-app.js
@@ -0,0 +1,59 @@
+// Initialize app
+var myApp = new Framework7();
+
+
+// If we need to use custom DOM library, let's save it to $$ variable:
+var $$ = Dom7;
+
+// Add view
+var mainView = myApp.addView('.view-main', {
+    // Because we want to use dynamic navbar, we need to enable it for this view:
+    dynamicNavbar: true,
+    domCache: true
+});
+
+// Handle Cordova Device Ready Event
+$$(document).on('deviceready', function() {
+    console.log("deviceready");
+});
+
+
+// Now we need to run the code that will be executed only for About page.
+
+// Option 1. Using page callback for page (for "about" page in this case) (recommended way):
+myApp.onPageInit('about', function (page) {
+    // Do something here for "about" page
+    console.log('onPageInit for about page');
+})
+
+// Option 2. Using one 'pageInit' event handler for all pages:
+$$(document).on('pageInit', function (e) {
+    // Get page data from event data
+    var page = e.detail.page;
+
+    if (page.name === 'about') {
+        // Following code will be executed for page with data-page attribute equal to "about"
+        console.log('Global pageInit handler with if statement');
+        // myApp.alert()
+    }
+})
+
+$$('.gettingStartedVideos').on('click', function () {
+    document.getElementById('videosLeft').style.display = 'block';
+    document.getElementById('mainLeft').style.display = 'none';
+});   
+
+$$('.gettingStartedVideosClose').on('click', function () {
+    document.getElementById('videosLeft').style.display = 'none';
+    document.getElementById('mainLeft').style.display = 'block';
+});
+
+$$('.printer01').on('click', function () {
+    mainView.router.loadPage('printer01.html');
+});
+
+// Option 2. Using live 'pageInit' event handlers for each page
+$$(document).on('pageInit', '.page[data-page="about"]', function (e) {
+    // Following code will be executed for page with data-page attribute equal to "about"
+    console.log('pageInit with selector for about');
+})
